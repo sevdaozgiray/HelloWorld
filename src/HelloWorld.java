8b566aaa3675347be9552a16e9190be72821020a
sozgiray <sevda.ozgiray@assistt.com.tr>
2021-11-16 17:59:19 +0300

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * @Author sevda
+ * @create 29.05.2021 20:48
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
+}
+
+
+
